b24ea147146e5b83b313266b43c008885f830a4a
njmarzina <njmarzina@gmail.com>
2025-04-23 15:36:55 -0400

    buttons to add/subtract

--- app/src/main/java/edu/temple/counter/MainActivity.kt
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,9 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CounterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Counter(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Counter(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -36,14 +32,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
-    Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+    var count by remember { mutableStateOf(0) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { count++ }) {
+            Image(
+                painter = painterResource(id = android.R.drawable.arrow_up_float),
+                contentDescription = "Add",
+                modifier = Modifier.size(24.dp)
+            )
+        }
+
         Text(
-            text = "0",
-            fontSize = 192.sp
+            text = count.toString(),
+            fontSize = 192.sp,
+            modifier = Modifier.padding(vertical = 16.dp)
         )
+
+        Button(onClick = { count-- }) {
+            Image(
+                painter = painterResource(id = android.R.drawable.arrow_down_float),
+                contentDescription = "Subtract",
+                modifier = Modifier.size(24.dp)
+            )
+        }
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
